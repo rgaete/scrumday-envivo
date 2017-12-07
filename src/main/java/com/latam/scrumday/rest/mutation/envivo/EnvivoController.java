8342fc381d5b3f0a62295c2572e666055a01bd09
Ricardo Gaete Duarte <ricardo.gaete@latam.com>
2017-12-07 17:39:21 -0300

    Adding circleci configuration

--- src/main/java/com/latam/scrumday/rest/mutation/envivo/EnvivoController.java
+++ src/main/java/com/latam/scrumday/rest/mutation/envivo/EnvivoController.java
@@ -1,5 +1,7 @@
 package com.latam.scrumday.rest.mutation.envivo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +12,12 @@ public class EnvivoController {
     @RequestMapping(method = RequestMethod.GET)
     public String getHola() {
         return "Hola Mundo";
+    }
+
+    @RequestMapping(path = "mutants/{id}", method = RequestMethod.GET)
+    public ResponseEntity getMutant(@PathVariable Integer id) {
+        Mutant mutant = new Mutant();
+        mutant.setName("scrumDay");
+        return ResponseEntity.ok(mutant);
     }
 }
---
+++ src/main/java/com/latam/scrumday/rest/mutation/envivo/Mutant.java
@@ -0,0 +1,31 @@
+package com.latam.scrumday.rest.mutation.envivo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Mutant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
